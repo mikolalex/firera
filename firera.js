1c75577c72e3ec1b0eaa4dcd02ac30645baf5944
mikolalex <mikolalex@online.ua>
2014-02-18 03:21:43 +0200

    first commit

---
+++ firera.js
@@ -0,0 +1,132 @@
+var Firera = function($el){
+    var $scope = $el || $(document);
+    var vars = [];
+    var modifiers = {
+	cell: {
+	    
+	},
+	visibility: {
+	    setter: function(val){
+		if(val){
+		    $(this.jquerySelector, $scope).show();
+		} else {
+		    $(this.jquerySelector, $scope).hide();
+		}
+	    }
+	},
+	val: {
+	    selfRefresh: true,
+	    setter: function(val){
+		$(this.jquerySelector, $scope).val(val);
+	    },
+	    getter: function(){
+		return $(this.jquerySelector, $scope).val();
+	    },
+	    startObserving: function(){
+		var self = this;
+		$(this.jquerySelector, $scope).keyup(function(val){
+		    self.val = val;
+		    self.compute();
+		})
+	    }
+	},
+	html: {
+	    setter: function(val){
+		$(this.jquerySelector, $scope).html(val);
+	    }
+	}
+    }
+    
+    var Cell = function(selector){
+	vars[selector] = this;
+	if(selector.indexOf("|") !== -1){
+	    // this is a dom selector
+	    var parts = selector.split("|");
+	    this.jquerySelector = parts[0];
+	    if(!modifiers[parts[1]]){
+		error('Unknown html modifier: ' + parts[1]);
+	    } else {
+		this.type = parts[1];
+	    }
+	    if(modifiers[this.type].selfRefresh){
+		this.compute = function(){
+		    this.val = modifiers[this.type].getter.apply(this);
+		    if(this.observers){
+			for(var i=0; i<this.observers.length;i++){
+			    //console.log('we call for observers of ' + func.toString() + ' : ' + i);
+			    this.observers[i].compute();
+			}
+		    } 
+		}
+		modifiers[this.type].startObserving.apply(this);
+		this.compute();
+	    }
+	} else { // this is just custom abstract varname
+	    this.type = 'cell';
+	}
+	this.observers = [];
+	this.modifier = modifiers[this.type];
+    }
+    
+    Cell.prototype.addObserver = function(cell){
+	this.observers.push(cell);
+    }
+    
+    Cell.prototype.get = function(){
+	return this.val;
+    }
+    
+    Cell.prototype.set = function(val){
+	this.val = val;
+	this.compute(true);
+	if(this.modifier.setter){
+	    this.modifier.setter.apply(this, val);
+	}
+	return this;
+    }
+    
+    Cell.prototype.as = function(cell){
+	this.is.call(this, function(flag){ return !!flag;}, cell);
+    }
+    Cell.prototype.notAs = function(cell){
+	this.is.call(this, function(flag){ return !flag;}, cell);
+    }
+    
+    Cell.prototype.is = function(func, vars){
+	var args = Array.prototype.slice.call(arguments, 1);
+	if(vars){
+	    for(var i= 0;i<args.length;i++){
+		args[i].addObserver(this);
+	    }
+	    this.func = func;
+	} else {
+	    this.val = func;
+	}
+	this.compute = function(skipSelf){
+	    if(!skipSelf){
+		var args1 = [];
+		for(var i=0; i<args.length;i++){
+		    args1.push(args[i].get());
+		}
+		//console.log('func is' + func);
+		this.val = func.apply(this, args1);
+		//console.log('we call ' + func.toString() + ' with ' + args1 + ' and set val to ' + this.val);
+		if(this.modifier.setter){
+		    this.modifier.setter.call(this, this.val);
+		}
+	    }
+	    if(this.observers){
+		for(var i=0; i<this.observers.length;i++){
+		    //console.log('we call for observers of ' + func.toString() + ' : ' + i);
+		    this.observers[i].compute();
+		}
+	    } 
+	}
+	if(vars) this.compute();
+	return this;
+    }
+    
+    return function(selector){
+	return vars[selector] || new Cell(selector);
+    }
+}
