9a4b04ddcb60be26f47acde4612e03a599c64429
mikolalex <mikolalex@ukr.net>
2016-06-10 10:54:54 +0300

    default tags for ozenfant: li for ul children etc

--- 2.0/ozenfant/ozenfant.js
+++ 2.0/ozenfant/ozenfant.js
@@ -51,30 +51,47 @@
 		}
 	}
 	
-	var toHTML = function(node, context){
-		var res1 = [], res2 = [], after = '';
-		for(let child of node.children){
-			res1.push(toHTML(child, context));
-		}
+	var toHTML = function(node, context, parent_tag){
 		var indent = `
 ` + new Array(node.level).join('	');
-		if(node.tagname || node.classnames){
+		var res1 = [], res2 = [], after = '';
+		if(node.tagname || node.classnames || !parent_tag){
 			// it's a node
-			var tag = node.tagname ? node.tagname : 'div';
-			//console.log('TAG', tag, node.classnames);
-			res2.push(indent + '<' + tag);
-			if(node.classnames && node.classnames.length > 1){
-				res2.push(' class="' + node.classnames.substr(1).replace(/\./g, " ") + '"');
-			}
-			res2.push('>');
-			if(node.variable){
-				var key = get_varname(node);
-				res2.push(indent + '	' + (context[key] !== undefined ? context[key] : ''));
+			var tag;
+			if(node.tagname){
+				tag = node.tagname;
 			} else {
-				res2.push(res1.join(' '));
+				switch(parent_tag){
+					case 'ol':
+					case 'ul':
+						tag = 'li';
+					break;
+					case 'tr':
+						tag = 'td';
+					break;
+					default:
+						tag = 'div';
+					break;
+				}
 			}
-			res2.push(indent + '</' + tag + '>');
-			return res2.join('');
+			for(let child of node.children){
+				res1.push(toHTML(child, context, tag));
+			}
+			if(parent_tag){
+				res2.push(indent + '<' + tag);
+				if(node.classnames && node.classnames.length > 1){
+					res2.push(' class="' + node.classnames.substr(1).replace(/\./g, " ") + '"');
+				}
+				res2.push('>');
+				if(node.variable){
+					var key = get_varname(node);
+					res2.push(indent + '	' + (context[key] !== undefined ? context[key] : ''));
+				} else {
+					res2.push(res1.join(' '));
+				}
+				res2.push(indent + '</' + tag + '>');
+				return res2.join('');
+			}
 		} else {
 			// its var of text node
 			if(node.quoted_str){
