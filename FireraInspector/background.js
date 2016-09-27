9cb95dd25b8f57ba0db6e4678719704da6d210bf
mikolalex <mikolalex@ukr.net>
2016-09-27 14:31:41 +0300

    bye-bye

--- FireraInspector/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(
-	function(message/*, sender, sendResponse*/) {
-		//console.log('got message', message);
-        chrome.tabs.executeScript(message.tabId, { file: message.scriptToInject });
-	}
-);
